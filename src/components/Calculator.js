2a959ac87770e7ff56cfa2ab8de91d93d9a9aa48
Munezero Christian <chamsf5@aol.com>
2022-04-21 15:51:15 +0200

    Refactor class component to functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,175 +1,93 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (buttonName) => {
-    this.setState((state) => calculate(state, buttonName));
+const Calculator = () => {
+  const initialValues = {
+    total: 0,
+    next: null,
+    operation: null,
   };
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="container">
-        <p>
-          <span>{total}</span>
-          <span>{operation}</span>
-          <span>{next}</span>
-        </p>
-        <div className="container-row">
-          <button
-            type="button"
-            name="AC"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            AC
-          </button>
-          <button
-            type="button"
-            name="+/-"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            +/-
-          </button>
-          <button
-            type="button"
-            name="%"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            %
-          </button>
-          <button
-            type="button"
-            name="÷"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            ÷
-          </button>
-        </div>
-        <div className="container-row">
-          <button
-            type="button"
-            name="7"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            7
-          </button>
-          <button
-            type="button"
-            name="8"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            8
-          </button>
-          <button
-            type="button"
-            name="9"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            9
-          </button>
-          <button
-            type="button"
-            name="*"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            *
-          </button>
-        </div>
-        <div className="container-row">
-          <button
-            type="button"
-            name="4"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            4
-          </button>
-          <button
-            type="button"
-            name="5"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            5
-          </button>
-          <button
-            type="button"
-            name="6"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            6
-          </button>
-          <button
-            type="button"
-            name="-"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            -
-          </button>
-        </div>
-        <div className="container-row">
-          <button
-            type="button"
-            name="1"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            1
-          </button>
-          <button
-            type="button"
-            name="2"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            2
-          </button>
-          <button
-            type="button"
-            name="3"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            3
-          </button>
-          <button
-            type="button"
-            name="+"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            +
-          </button>
-        </div>
-        <div className="container-row">
-          <button
-            type="button"
-            name="0"
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            0
-          </button>
-          <button
-            type="button"
-            name="."
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            .
-          </button>
-          <button
-            type="button"
-            name="="
-            onClick={(e) => this.handleClick(e.target.name)}
-          >
-            =
-          </button>
-        </div>
+  const [values, setValues] = useState(initialValues);
+  const handleClick = (e) => {
+    setValues({ ...values, ...calculate(values, e.target.name) });
+  };
+  const { total, operation, next } = values;
+  return (
+    <div className="container">
+      <p>
+        <span>{total}</span>
+        <span>{operation}</span>
+        <span>{next}</span>
+      </p>
+      <div className="container-row">
+        <button type="button" name="AC" onClick={handleClick}>
+          AC
+        </button>
+        <button type="button" name="+/-" onClick={handleClick}>
+          +/-
+        </button>
+        <button type="button" name="%" onClick={handleClick}>
+          %
+        </button>
+        <button type="button" name="÷" onClick={handleClick}>
+          ÷
+        </button>
       </div>
-    );
-  }
-}
+      <div className="container-row">
+        <button type="button" name="7" onClick={handleClick}>
+          7
+        </button>
+        <button type="button" name="8" onClick={handleClick}>
+          8
+        </button>
+        <button type="button" name="9" onClick={handleClick}>
+          9
+        </button>
+        <button type="button" name="*" onClick={handleClick}>
+          *
+        </button>
+      </div>
+      <div className="container-row">
+        <button type="button" name="4" onClick={handleClick}>
+          4
+        </button>
+        <button type="button" name="5" onClick={handleClick}>
+          5
+        </button>
+        <button type="button" name="6" onClick={handleClick}>
+          6
+        </button>
+        <button type="button" name="-" onClick={handleClick}>
+          -
+        </button>
+      </div>
+      <div className="container-row">
+        <button type="button" name="1" onClick={handleClick}>
+          1
+        </button>
+        <button type="button" name="2" onClick={handleClick}>
+          2
+        </button>
+        <button type="button" name="3" onClick={handleClick}>
+          3
+        </button>
+        <button type="button" name="+" onClick={handleClick}>
+          +
+        </button>
+      </div>
+      <div className="container-row">
+        <button type="button" name="0" onClick={handleClick}>
+          0
+        </button>
+        <button type="button" name="." onClick={handleClick}>
+          .
+        </button>
+        <button type="button" name="=" onClick={handleClick}>
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
